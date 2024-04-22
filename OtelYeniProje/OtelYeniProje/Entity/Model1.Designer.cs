bbe052c681fe140e6b925c88b1e734f6aeaa4509
oructuran0 <orctrn193@gmail.com>
2024-04-22 23:08:57 +0300

    Sql Tablo Guncellemesi
    sql tablosunda admin kisminda rol sutununun veri tipi char dan nvarchar a donusturuldu

--- OtelYeniProje/OtelYeniProje/Entity/Model1.Designer.cs
+++ OtelYeniProje/OtelYeniProje/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\orctr\source\repos\OtelYeniProje\OtelYeniProje\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GitHub\OtelRezervasyonSistemi-DesktopApp\OtelYeniProje\OtelYeniProje\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
